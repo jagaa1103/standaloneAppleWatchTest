ec9d97320f2923a7f05e4bda4c87d36ea85d8195
Jagaa <jagaa1103@gmail.com>
2016-03-10 16:20:39 +0900

    init Project

---
+++ standaloneAppleWatchTest/standaloneAppleWatchTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  standaloneAppleWatchTest
+//
+//  Created by Enkhjargal Gansukh on 3/10/16.
+//  Copyright © 2016 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
